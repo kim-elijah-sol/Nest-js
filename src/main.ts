8063feeb19a0d002b05e4aa8389535ef4972fa8e
kim-elijah-sol <kimelijahsol@gmail.com>
2025-03-14 14:10:55 +0000

    Swagger 문서 생성

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './utils/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,9 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
+  setupSwagger(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
